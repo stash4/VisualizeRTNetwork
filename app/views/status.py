9b35f7f08dc2c8fd0a501eae69f4427103bfcea3
stash4 <high.mackey@gmail.com>
2018-02-08 11:51:43 +0900

    status.pyに埋め込みツイート用のメソッド追加

--- app/views/status.py
+++ app/views/status.py
@@ -1,4 +1,21 @@
 from flask import Blueprint, render_template
+import requests
+
+params = {
+    "hide_thread": "true,"
+    "dnt": "true"
+}
+
+
+def oembed_tweet(id, params=params):
+    '''
+    埋め込みツイートのマークアップを取得
+    '''
+    params['url'] = f'https://twitter.com/user/status/{id}'
+    url = 'https://publish.twitter.com/oembed'
+    res = requests.get(url, params=params)
+    return res.json()['html']
+
 
 status = Blueprint('status', __name__)
 
