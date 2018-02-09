77b3e030091cea906ac3ed9964ab140e463deecf
マッシュ <massiyu0@yahoo.co.jp>
2018-02-09 23:48:37 +0900

    connection_listを設定する関数を追加等

--- app/rtanalysis/AnalyzeRTData.py
+++ app/rtanalysis/AnalyzeRTData.py
@@ -1,9 +1,44 @@
 import tweepy
 
 
-def get_user_data(api, tid, retweeters_data_list):
+def trace_tree(retweeters_id_list): # ここで再帰処理を行う(予定)
+    # retweeters_id_list = []
+    for rid in retweeters_id_list:
+        followers_ids = tweepy.Cursor(api.followers_ids, user_id=rid).pages()
+        followers_id_list = []
+
+        for followers_id in followers_ids:
+            followers_id_list.append(followers_id)
+        print(len(followers_id_list))
+        print(followers_id_list[0])
+        followers_id_list2 = followers_id_list[0]
+
+        fler_set = set(followers_id_list2)  # set化
+        retweeters_id_list = list(rter_set & fler_set)  # 2つのsetの論理積を取って共通の値を取得
+        for mlist in matched_list:
+            print(mlist)
+    print(str(stage) + "段階目終了")
+    stage += 1
+    # if len(matched_list) == 0:
+
+
+def set_connection(ruser, retweeters_id_list):
+    for rlist in retweeters_id_list:
+        if rlist.distance != -1:
+            ruser.connection_list.append([rlist.user_id, ruser.distance + rlist.distance])
+        else:
+            ruser.connection_list.append([rlist.user_id, ruser.distance + 1])
+
+    # データ確認用
+    '''
+    for rcnctn in ruser.connection_list:
+        print(rcnctn)
+    '''
+
+
+def get_user_data(api, ruser, retweeters_data_list):
     # フォロワーのidを取得。一度に5000人まで取得できるらしい。15分15回
-    followers_ids = tweepy.Cursor(api.followers_ids, user_id=tid).pages()
+    followers_ids = tweepy.Cursor(api.followers_ids, user_id=ruser.user_id).pages()
 
     # followers_idsはtweepy.cursor.CursorIterator型になってるのでlistに変換
     followers_id_list = []
@@ -17,41 +52,32 @@ def get_user_data(api, tid, retweeters_data_list):
         retweeters_id_list.append(int(rlist.user_id))
 
     # フォロワーの中でRTした人を取得
-    print("1段階目開始")
     rter_set = set(retweeters_id_list)  # set化
     fler_set = set(followers_id_list2)  # set化
     matched_list = list(rter_set & fler_set)  # 2つのsetの論理積を取って共通の値を取得
     for mlist in matched_list:
         print(mlist)  # ツイート主のフォロワーで且つRTした人だけを出力
-    print("1段階目終了")
+    print("----------")
 
-    # RTした人から同様の処理でさらに繋がりを探していく
+    # RTData型に戻す
+    retweeters_data_list2 = []
+    for mlist in matched_list:
+        for rdlist in retweeters_data_list:
+            if mlist == rdlist.user_id:
+                retweeters_data_list2.append(rdlist)
+
+    # データ確認用
     '''
-    この処理が再帰的であることに気付いたところなので書き直す予定
-    ちなみにこれだと動かない
-    stage = 2
-    while(True):
-        print(str(stage) + "段階目開始")
-        retweeters_id_list = []
-        for rid in matched_list:
-            followers_ids = tweepy.Cursor(api.followers_ids, user_id=rid).pages()
-            followers_id_list = []
-
-            for followers_id in followers_ids:
-                followers_id_list.append(followers_id)
-            print(len(followers_id_list))
-            print(followers_id_list[0])
-            followers_id_list2 = followers_id_list[0]
-
-            fler_set = set(followers_id_list2)  # set化
-            retweeters_id_list = list(rter_set & fler_set)  # 2つのsetの論理積を取って共通の値を取得
-            for mlist in matched_list:
-                print(mlist)
-        print(str(stage) + "段階目終了")
-        stage += 1
-        if len(matched_list) == 0:
-            break
+    for rd2list in retweeters_data_list2:
+        print(rd2list.user_id)
+        print(rd2list.user_name)
     '''
+
+    # つながりを設定
+    set_connection(ruser, retweeters_data_list2)
+
+    # RTした人から同様の処理で再帰的に繋がりを探していく
+    # trace_tree(retweeters_data_list2)
 
 
 
