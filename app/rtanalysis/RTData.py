f6fa45da27e42cdceb8646ee620a27c542c5ddb1
マッシュ <massiyu0@yahoo.co.jp>
2018-02-09 23:52:33 +0900

    groupとdistanceを要素に追加

---
+++ app/rtanalysis/RTData.py
@@ -0,0 +1,8 @@
+class RTData:  # エラー起きたのでやっぱり別で
+    def __init__(self, user_id, status_id, user_name, distance=-1, group=-1, connection_list=[]):
+        self.user_id = user_id
+        self.status_id = status_id
+        self.user_name = user_name
+        self.distance = distance
+        self.group = group
+        self.connection_list = connection_list
