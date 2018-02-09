04f23a0a03fd6f6750c9fa5b930f23441ad1ccdc
マッシュ <massiyu0@yahoo.co.jp>
2018-02-09 23:51:52 +0900

    RTDataの要素追加に伴うインスタンス生成時の情報追加

--- app/rtanalysis/GetRTConnection.py
+++ app/rtanalysis/GetRTConnection.py
@@ -1,13 +1,15 @@
-import GetAndAnalyzeRTMain
+import RTData
 
 
 def get_rt_ids(api, sid):
+    group_id = 0
     retweeter = api.retweets(sid, 100)  # RTしたユーザを取得(100件)
     retweeter_data = []  # RTData格納用
 
     for rter in retweeter:
-        retweeter_data.append(GetAndAnalyzeRTMain.RTData(rter.user.id, sid, rter.user.name))  # 情報を抽出してRTData型として管理
-        # user_idを基に他の情報を調べる手法だとかなり時間がかかるため、つながりデータは後回しにしてRTDataを生成した。
+        retweeter_data.append(RTData.RTData(rter.user.id, sid, rter.user.name, 1, group_id))  # 情報を抽出してRTData型として管理
+        group_id += 1
+        # user_idを基に他の情報を調べる手法だと応答にかなり時間がかかるため、つながりデータは後回しにしてRTDataを生成した。
 
     return retweeter_data
 
