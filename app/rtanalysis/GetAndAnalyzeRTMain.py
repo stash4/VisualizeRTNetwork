89f196dbcb9e3a77b33054fb3bc988a3e7752c2f
マッシュ <massiyu0@yahoo.co.jp>
2018-02-09 23:49:58 +0900

    エラーが起きるのでRTDataクラスを別のモジュールに入れた

--- app/rtanalysis/GetAndAnalyzeRTMain.py
+++ app/rtanalysis/GetAndAnalyzeRTMain.py
@@ -2,14 +2,7 @@ import AnalyzeRTData
 import GetRTConnection
 import tweepy
 import TwitterKey
-
-
-class RTData:  # このモジュールにまとめたほうがコンパクトそうなのでここに入れてみた
-    def __init__(self, user_id, status_id, user_name, connection_list=[]):  # RT時刻はjsonデータを見る限り取得出来ていなかったのでひとまず無しで
-        self.user_id = user_id
-        self.status_id = status_id
-        self.connection_list = connection_list
-        self.user_name = user_name
+import RTData
 
 
 def get_rt_data(give_api, sid):
@@ -20,7 +13,7 @@ def analyze_rt(give_api, tid, rlist):
     AnalyzeRTData.get_user_data(give_api, tid, rlist)
 
 
-def gaa_main(url="https://twitter.com/jr_tduniv/status/877352450398732292"):  # 初期値はとりあえずイタリアントマト
+def gaa_main(url="https://twitter.com/jr_tduniv/status/877352450398732292"):  # 初期値はイタリアントマト
     # TwitterAPIの認証作業
     auth = tweepy.OAuthHandler(TwitterKey.CONSUMER_KEY, TwitterKey.CONSUMER_SECRET_KEY)
     auth.set_access_token(TwitterKey.ACCESS_TOKEN, TwitterKey.ACCESS_TOKEN_SECRET)
@@ -28,9 +21,13 @@ def gaa_main(url="https://twitter.com/jr_tduniv/status/877352450398732292"):  # 
 
     # ツイートからツイートidとツイート主のuserIDを取得
     status_id = url.split("/")[5]  # ツイートID
+
     tweeter_screen_name = url.split("/")[3]  # スクリーンネーム
     tweeter_data = api.get_user(tweeter_screen_name)  # スクリーンネームからツイート主の情報を取得
-    tweeter_id = tweeter_data.id  # その中からuserIDだけ抜き出す
+    tweeter_id = tweeter_data.id  # その中からuserIDを抜き出す
+    tweeter_name = tweeter_data.name  # ユーザ名も抜き出す
+    root_user = RTData.RTData(tweeter_id, status_id, tweeter_name, 0)  # RTData型で管理する
+    print(root_user.user_name)
 
     # RTしたユーザの情報を取得(100件)
     retweeter_data_list = get_rt_data(api, status_id)
@@ -45,7 +42,7 @@ def gaa_main(url="https://twitter.com/jr_tduniv/status/877352450398732292"):  # 
     '''
 
     # 取得した情報からつながりを分析してデータを返す
-    return analyze_rt(api, tweeter_id, retweeter_data_list)
+    return analyze_rt(api, root_user, retweeter_data_list)
 
 
 gaa_main()
