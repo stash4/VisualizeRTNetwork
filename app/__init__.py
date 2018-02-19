b046f17c1ae9b59b27983b1358fce1209377fceb
stash4 <high.mackey@gmail.com>
2018-02-19 23:38:50 +0900

    config.pyを追加

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from .views.status import status
 
 def init_app():
     app = Flask(__name__)
+    app.config.from_object('app.config')
     app.register_blueprint(status, url_prefix='/status')
 
     return app
