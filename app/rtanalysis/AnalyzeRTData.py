3d071699affb3cefbb15985ac68a2db7bd51a1b8
マッシュ <massiyu0@yahoo.co.jp>
2018-02-20 18:37:22 +0900

    データをdict型で返すように変更

--- app/rtanalysis/AnalyzeRTData.py
+++ app/rtanalysis/AnalyzeRTData.py
@@ -1,12 +1,25 @@
 import tweepy
+import json
+import ast
+import collections
+import datetime
 
 retweeter_tree = []
 node_id = set()
 
 
+def check_api_limit(api):
+    print("API残り呼び出し回数：" + str(api.rate_limit_status("followers")["resources"]["followers"]["/followers/ids"]["remaining"]) + "回")
+    if api.rate_limit_status("followers")["resources"]["followers"]["/followers/ids"]["remaining"] == 0:
+        print("API制限に引っかかりました(◞‸◟)")
+        print("次の開始時間" + str(datetime.datetime.fromtimestamp(api.rate_limit_status("followers")["resources"]["followers"]["/followers/ids"]["reset"])))
+
+
 def get_follower_ids(api, user_id):
     followers_ids = tweepy.Cursor(api.followers_ids, user_id=user_id).pages()  # フォロワーのidを取得。一度に5000人まで取得できるらしい。15分15回
     followers_id_list = []
+
+    check_api_limit(api)  # API制限の回数や制限に引っかかった時の再開時間を教えてくれる
 
     # followers_idsはtweepy.cursor.CursorIterator型になってるのでlistに変換
     for followers_id in followers_ids:
@@ -76,7 +89,7 @@ def trace_tree(api, retweeters_data_list, root_retweeters_data_list, tree):
         # ユーザをフォロー&ツイートをRTしたユーザのリストを取得(RTData型)
         retweeters_data_list2 = get_retweeter_data(root_retweeters_data_list, followers_id_list)
         if len(retweeters_data_list2) == 0:
-            print("子ノード無し")
+            print("子ノード：無し")
         for rd in retweeters_data_list2:
             print("子ノード：" + rd.user_name)
 
@@ -97,9 +110,29 @@ def trace_tree(api, retweeters_data_list, root_retweeters_data_list, tree):
         trace_tree(api, retweeters_data_list4, root_retweeters_data_list, tree+1)
 
 
-def analyze_main(api, ruser, root_retweeters_data_list):
+def create_dict(tree, stext):
+    # json風に変換
+    data_str = "{"
+    data_str += "\"tweetid\":" + str(tree[0].status_id) + ","
+    data_str += "\"text\":\"" + stext + "\","
+    data_str += "\"users\":["
+    for trid in tree:
+        data_str += "{\"userid\":" + str(trid.user_id) + ", \"name\":\"" + str(trid.user_name) + "\", \"group\":" + str(trid.group) + "},"
+    data_str += "], \"links\":["
+    for trlink in tree:
+        for trcnt in trlink.connection_list:
+            data_str += "{\"source\":" + str(trlink.user_id) + ", \"target\":" + str(trcnt[0]) + ", \"distance\":" + str(trcnt[1]) + "},"
+    data_str += "]"
+    data_str += "}"
+
+    # dict型に変換してreturn
+    dic = collections.OrderedDict(ast.literal_eval(data_str))
+    print(json.dumps(dic, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': ')))  # 整形したものを表示
+    return dic
+
+
+def analyze_main(api, ruser, root_retweeters_data_list, status_text):
     # 再帰的に繋がりを探していく
     retweeters_data_list = [ruser]
-    node_id.add(ruser.user_id)
     trace_tree(api, retweeters_data_list, root_retweeters_data_list, 1)
-    return retweeter_tree
+    return create_dict(retweeter_tree, status_text)
