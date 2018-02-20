f99ca1968c9c20e129a53d18470324bc7f1c44a2
sakari1013 <33144516+sakari1013@users.noreply.github.com>
2018-02-20 23:23:39 +0900

    Add files via upload

--- app/RTDataDAO.py
+++ app/RTDataDAO.py
@@ -1,25 +1,55 @@
+from app import db
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import GetAndAnalyzeRTMain as Artmain
-import GetRTConnection as rtcone
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sampledb.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////magical.db'
 db = SQLAlchemy(app)
 
-class Tweet_info(db.Model):#tweet情報を保存
-    tweet = [rtcone.status_id]##,tweet内容]
-    db.session().add_all(tweet)
-    db.session().commit()
+
+class Tweets(db.Model):
+    __tweet_info__= 'tweets'
+    id = db.Column(db.Integer,primary_key=True)
+    text = db.Column(db.Text)
+
+    def __int__(self, id, text):
+        self.id = id
+        self.text = text
+
+    def __repr__(self):
+        return f'<Tweet id={self.id} text={self.text}>'
+
+class User(db.Model):
+    __user_info__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Name)
+
+    def __int__(self, id, name):
+       self.id = id
+       self.name = name
+
+    def __repr__(self):
+       return f'<User id={self.id} name={self.name}>'
+
+class Connection(db.Model):
+   __connection_tweets__ = 'connection'
+   target_ids = db.Coulmn(db.Integer,db.Integer)
+   group = db.Coulmn(db.Integer)
+   value = db.Coulmn(db.Integer)
+
+   def __int__(self, tid, uid, target_ids, group, value):
+       self.tid = Tweets.id
+       self.uid = User.id
+       self.target_ids = target_ids
+       self.group = group
+       self.value = value
+
+   def __repr__(self):
+       return f'<Connection tid={Tweets.id} uid={User.id}' \
+              f'target={self.target_ids} group={self.group}' \
+              f'value={self.value}>'
 
 
-class User_info(db.Model):#user情報を保存
-    user = [Artmain.rtree.user_id,Artmain.rtree.user_name]
-    db.session().add_all(user)
-    db.session().commit()
-
-class Connection_tweet(db.Model):##繋がり情報を保存
-    connnection = [Artmain.rtree.user_id,rtcone.status_id
-        ,Artmain.ist.user_id,Artmain.rtree.distance]
-    db.session().add_all(connnection)
-    db.session().commit()
+def register(retweeter_tree):
+    db.session.add()
+    db.session.commit()
