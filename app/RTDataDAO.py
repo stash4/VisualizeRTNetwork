0256cdf7a00e4d159d74a5eebb0a3a2339d4b9ac
stash4 <high.mackey@gmail.com>
2018-02-21 23:55:49 +0900

    model定義

--- app/RTDataDAO.py
+++ app/RTDataDAO.py
@@ -1,55 +1,62 @@
+import uuid
 from app import db
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////magical.db'
-db = SQLAlchemy(app)
 
 
-class Tweets(db.Model):
-    __tweet_info__= 'tweets'
-    id = db.Column(db.Integer,primary_key=True)
+class Tweet(db.Model):
+    __tablename__ = 'tweets'
+    id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text)
+    users = db.relationship('User', backref='tweets', lazy=True)
+    links = db.relationship('Link', backref='tweets')
 
-    def __int__(self, id, text):
+    def __init__(self, id, text):
         self.id = id
         self.text = text
 
     def __repr__(self):
         return f'<Tweet id={self.id} text={self.text}>'
 
-class User(db.Model):
-    __user_info__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Name)
 
-    def __int__(self, id, name):
-       self.id = id
-       self.name = name
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    tweet_id = db.Column(db.Integer, db.ForeignKey('tweets.id'),
+                         primary_key=True)
+    name = db.Column(db.Text)
+    group = db.Column(db.Integer)
+
+    def __init__(self, id, tweet_id, name, group):
+        self.id = id
+        self.tweet_id = tweet_id
+        self.name = name
+        self.group = group
 
     def __repr__(self):
-       return f'<User id={self.id} name={self.name}>'
-
-class Connection(db.Model):
-   __connection_tweets__ = 'connection'
-   target_ids = db.Coulmn(db.Integer,db.Integer)
-   group = db.Coulmn(db.Integer)
-   value = db.Coulmn(db.Integer)
-
-   def __int__(self, tid, uid, target_ids, group, value):
-       self.tid = Tweets.id
-       self.uid = User.id
-       self.target_ids = target_ids
-       self.group = group
-       self.value = value
-
-   def __repr__(self):
-       return f'<Connection tid={Tweets.id} uid={User.id}' \
-              f'target={self.target_ids} group={self.group}' \
-              f'value={self.value}>'
+        return f'<User id={self.id} tweet_id={self.tweet_id} '\
+            f'name={self.name} group={self.group}>'
 
 
-def register(retweeter_tree):
-    db.session.add()
-    db.session.commit()
+class Link(db.Model):
+    __tablename__ = 'links'
+    uuid = db.Column(db.Text, primary_key=True)
+    tweet_id = db.Column(db.Integer, db.ForeignKey('tweets.id'))
+
+    source_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    target_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    source = db.relationship('User', foreign_keys=[source_id])
+    target = db.relationship('User', foreign_keys=[target_id])
+
+    distance = db.Column(db.Integer)
+
+    def __init__(self, tweet_id, source_id, target_id, distance):
+        self.uuid = uuid.uuid4()
+        self.tweet_id = tweet_id
+        self.source_id = source_id
+        self.target_id = target_id
+        self.distance = distance
+
+    def __repr__(self):
+        return f'<Link uuid={self.uuid} tweet_id={self.tweet_id} '\
+            f'source_id={self.source_id} target_id={self.target_id} '\
+            f'distance={self.distance}>'
