668d52058d1f38da4624b01a76571ad2ef79385f
stash4 <high.mackey@gmail.com>
2018-02-26 00:00:06 +0900

    db_jobを修正

--- app/worker.py
+++ app/worker.py
@@ -2,14 +2,15 @@ import os
 from apscheduler.schedulers.background import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from models import Tweet
-from rtanalysis.RTDataDAO import db, register
+from rtanalysis.RTDataDAO import db
+from rtanalysis.GetAndAnalyzeRTMain import gaa_main
 
 
 def db_job():
     tweets = db.session.query(Tweet).all()
     for tw in tweets:
         if not tw.user and not tw.links:
-            register(tw.id)
+            gaa_main(f'https://twitter.com/user/status/{tw.id}')
 
 
 scheduler = BlockingScheduler(standalone=True, coalesce=True)
