212c0863efdbbd5b5d48bdb93418b34d42de59c4
stash4 <high.mackey@gmail.com>
2018-02-25 16:45:53 +0900

    statusのimportのタイミングを修正

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .views.status import status
 
 app = Flask(__name__)
 app.config.from_object('app.config')
-app.register_blueprint(status, url_prefix='/status')
 
 db = SQLAlchemy(app)
+
+from .views.status import status
+app.register_blueprint(status, url_prefix='/status')
